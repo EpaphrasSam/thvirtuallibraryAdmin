8a97e17dd85d3fdc0a1f4be024f0951c262d0250
EpaphrasSam <108902602+EpaphrasSam@users.noreply.github.com>
2022-11-14 01:30:05 +0000

    Revert "Admin Site"
    This reverts commit 894fd681342fd58ce61a1b73f76753a5f484b2e7.

--- src/AdminDashboard/axios.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios").default;
-
-export default axios.create(
-    {
-    // baseURL: "http://www.thvirtuallibrary.com:3500",
-    baseURL: "http://localhost:3500",
-    withCredentials: true
-});
--- src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios").default;
-
-export default axios.create(
-    {
-        baseURL: "https://evening-springs-84078.herokuapp.com",
-        // baseURL: "http://localhost:3500",
-        withCredentials: true
-    });
