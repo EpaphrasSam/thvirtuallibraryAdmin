a8eb02357843cc544853e72e471ef75fc8a6b1da
EpaphrasSam <108902602+EpaphrasSam@users.noreply.github.com>
2022-11-15 14:26:36 +0000

    Updates to Admin Dashboard

--- src/AdminDashboard/Sidebar/Sidebar1.js
+++ src/AdminDashboard/Sidebar/Sidebar1.js
@@ -3,7 +3,7 @@ import { SidebarBox, NavBar, SidebarMenu, SidebarProfileIcon, NavLink } from "./
 import AdminProfile from "../../Images/Admin-pana.svg";
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-// import * as FiIcons from 'react-icons/fi';
+import * as FcIcons from 'react-icons/fc';
 
 function Sidebar1(props) {
 
@@ -47,7 +47,10 @@ function Sidebar1(props) {
                     <span className="title" style={{ display: props.isopen ? "block" : "none" }}>Delete File</span>
                 </NavLink>
 
-
+                <NavLink to="/dashboard/course" >
+                    <span className="icon" style={{ fontSize: props.isopen ? "20px" : "2rem" }}><AiIcons.AiFillBook /></span>
+                    <span className="title" style={{ display: props.isopen ? "block" : "none" }}>All Programmes</span>
+                </NavLink>
 
                 <NavLink to="/logout">
                     <span className="icon" style={{ fontSize: props.isopen ? "20px" : "2rem" }}><AiIcons.AiOutlineLogout /></span>
